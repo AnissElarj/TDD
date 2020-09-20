4c886467bb8efcabfac18dab6a8797d6807f9980
AnissElarj <36004330@parisnanterre.fr>
2020-09-20 17:10:27 +0200

    Data Provider Win

--- src/test/java/fr/parisnanterre/miage/rps/model/RockPaperScissorsTest.java
+++ src/test/java/fr/parisnanterre/miage/rps/model/RockPaperScissorsTest.java
@@ -2,10 +2,7 @@ package fr.parisnanterre.miage.rps.model;
 
 
 
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
+import org.testng.annotations.*;
 
 import static fr.parisnanterre.miage.rps.model.Result.LOST;
 
@@ -13,6 +10,7 @@ import static fr.parisnanterre.miage.rps.model.RPSEnum.*;
 
 import static fr.parisnanterre.miage.rps.model.RPSEnum.ROCK;
 import static fr.parisnanterre.miage.rps.model.RPSEnum.PAPER;
+import static fr.parisnanterre.miage.rps.model.Result.TIE;
 import static org.junit.Assert.assertEquals;
 
 import org.testng.asserts.*;
@@ -32,14 +30,29 @@ public class RockPaperScissorsTest {
 
     /* it's me mario 2  */
 
-    @Parameters({"papier","pierre"})
+   // @Parameters({"papier","pierre"})
 
-    @Test
+    @DataProvider(name="winData")
+    public Object[][] createWinData(){
+        return new Object[][]{
+                {rps.Play(ROCK,SCISSORS)},
+                {rps.Play(PAPER,ROCK)},
+                {rps.Play(SCISSORS,PAPER)}
+        };
+    }
+
+    @Test(dataProvider = "winData")
+
+    /*
     public void testWinPlay(String p1, String p2) {
         //assertEquals(rps.play(Play.valueOf("PAPER"), Play.valueOf("ROCK")), TIE)
-        assertEquals(rps.Play(ROCK,ROCK), LOST);
-
-
+        assertEquals(rps.Play(PAPER,ROCK), LOST);
     }
+
+    public void testTiePlay(String p1, String p2) {
+        assertEquals(rps.Play(ROCK,ROCK), TIE);
+    }
+    */
+
 
 }
