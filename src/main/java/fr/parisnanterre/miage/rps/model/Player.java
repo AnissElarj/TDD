9287b9e842377ae3f5032b853128fa736ca32c95
AnissElarj <36004330@parisnanterre.fr>
2020-09-21 22:39:24 +0200

    Player

---
+++ src/main/java/fr/parisnanterre/miage/rps/model/Player.java
@@ -0,0 +1,15 @@
+package fr.parisnanterre.miage.rps.model;
+
+import java.util.ArrayList;
+
+public class Player {
+
+    public String Nom;
+    public int Score;
+    public ArrayList <RPSEnum> Liste ;
+
+    public void construct (String nom, ArrayList<RPSEnum> liste){}
+    public void construct2 (String nom){
+
+    }
+}
